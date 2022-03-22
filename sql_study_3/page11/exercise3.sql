fa3b7f9d39690fa631e9f4272f32755ac077268d
yunitarsari <59533332+yrtnsari@users.noreply.github.com>
2022-03-23 00:17:48 +0700

    Add files via upload

---
+++ sql_study_3/page11/exercise3.sql
@@ -0,0 +1,8 @@
+SELECT
+FROM players
+SELECT countries.name AS "nama negara", AVG(goals) AS "skor rata-rata" 
+FROM players
+JOIN countries
+ON players.country_id = countries.id
+GROUP BY countries.name
+;
